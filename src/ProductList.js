aad8ebf205e290b4e9724519f762fcb3a0d0bc37
dfjod <daavisff@gmail.com>
2023-04-14 15:13:39 +0300

    003

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,56 +1,108 @@
 import { useState } from "react";
 
 let List = ({ products }) => {
-
   const [checkedProducts, setCheckedProducts] = useState([]);
 
   const handleCheckboxChange = (event) => {
     const { value, checked } = event.target;
 
     if (checked) {
-      setCheckedProducts(prev => [...prev, value])
+      setCheckedProducts((prev) => [...prev, value]);
     } else {
-      setCheckedProducts(prev => prev.filter(id => id != value))
+      setCheckedProducts((prev) => prev.filter((id) => id !== value));
     }
     console.log(checkedProducts);
-  }
+  };
+
+  const setAttribute = (product) => {
+    if (product.category == "FURNITURE") {
+      return (
+        <p>
+          {product.height}X{product.width}X{product.lenght}
+        </p>
+      );
+    } else if (product.category == "DVD") {
+      return <p>{product.size} MB</p>;
+    } else {
+      return <p>{product.weight} KG</p>;
+    }
+  };
 
   return (
     <div id="product-list">
-      {products.map(product => {
+      {products.map((product) => {
         return (
           <div className="product" key={product.id}>
-            <input type="checkbox" value={product.id} onChange={handleCheckboxChange} />
+            <input
+              type="checkbox"
+              value={product.id}
+              onChange={handleCheckboxChange}
+            />
             <p>{product.sku}</p>
             <p>{product.name}</p>
             <p>{product.price} $</p>
+            {setAttribute(product)}
           </div>
-        )
+        );
       })}
     </div>
-  )
-}
+  );
+};
 
 function ProductList() {
   const data = [
     {
-      id: 1, sku: "XJ93YH25DQ", name: "Old DVD", price: "11"
+      id: 1,
+      sku: "XJ93YH25DQ",
+      name: "Old DVD",
+      price: "11",
+      category: "DVD",
+      size: 20,
     },
     {
-      id: 2, sku: "L8V4TG01RS", name: "New book", price: "23"
+      id: 2,
+      sku: "L8V4TG01RS",
+      name: "New book",
+      price: "23",
+      category: "BOOK",
+      weight: 0.7,
     },
     {
-      id: 3, sku: "R5F7WQ68ZE", name: "Dusty table", price: "1"
+      id: 3,
+      sku: "R5F7WQ68ZE",
+      name: "Dusty table",
+      price: "1",
+      category: "FURNITURE",
+      height: 10,
+      width: 20,
+      lenght: 30,
     },
     {
-      id: 4, sku: "K2N8MU35IA", name: "Crucked chair", price: "4"
+      id: 4,
+      sku: "K2N8MU35IA",
+      name: "Crucked chair",
+      price: "4",
+      category: "FURNITURE",
+      height: 5,
+      width: 10,
+      lenght: 10,
     },
     {
-      id: 5, sku: "B0C9XJ21SW", name: "Scraped DVD", price: "1"
+      id: 5,
+      sku: "B0C9XJ21SW",
+      name: "Scraped DVD",
+      price: "1",
+      category: "DVD",
+      size: 256,
     },
     {
-      id: 6, sku: "B0C9XJ21SW", name: "Bomb ass book", price: "5"
-    }
+      id: 6,
+      sku: "B0C9XJ21SW",
+      name: "Bomb ass book",
+      price: "5",
+      category: "BOOK",
+      weight: 1,
+    },
   ];
   return (
     <>
@@ -67,7 +119,7 @@ function ProductList() {
       <hr />
       <p>Scandiweb test assignment</p>
     </>
-  )
+  );
 }
 
 export default ProductList;
